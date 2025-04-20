3a4c00a5de6778dd15e63f2a14087c75e5c018b7
DESKTOP-FET27VO\User <Oleg.s2822@gmail.com>
2025-04-20 18:50:08 +0300

    feat: add GoogleAuthButton and complete auth page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { ThemeProvider } from "./components/themes/context/ThemeContext"
 import BasicDataRouter from "./routes/BasicDataRouter"
-import GoogleAuthAppProvider from "./features/authorization/providers/GoogleAuthAppProvider"
 
 function App() {
   return (
-    <GoogleAuthAppProvider>
-      <ThemeProvider>
-        <BasicDataRouter />
-      </ThemeProvider>
-    </GoogleAuthAppProvider>
+    <ThemeProvider>
+      <BasicDataRouter />
+    </ThemeProvider>
   )
 }
 
