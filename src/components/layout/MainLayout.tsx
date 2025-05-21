9214f3024c491a2ff9e84b7b2cf42628e47dbcaf
DESKTOP-FET27VO\User <Oleg.s2822@gmail.com>
2025-05-22 00:32:06 +0300

    feat: little changes

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -5,22 +5,16 @@ const Layout = () => {
   return (
     <div className="dark:bg-gray-900 bg-gray-100 transition-colors duration-300 pb-16 md:pb-0">
       
-    
-
      <DashboardNavBar />
       <div className="flex-1 min-h-screen">
         <div className="container mx-auto p-8">
-       
-          <div className="flex flex-wrap">
-            
-            
+          <div className="flex flex-wrap">      
             <div className="w-full">
               <Outlet />
             </div>
           </div>
         </div>
       </div>
-      {/* <BottomNav /> */}
     </div>
   );
 }
