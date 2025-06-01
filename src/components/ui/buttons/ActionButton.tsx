18ef41d75d638508e60cf8a229d39e53f7219245
DESKTOP-FET27VO\User <Oleg.s2822@gmail.com>
2025-06-01 22:48:07 +0300

    feat: minor changes

--- src/components/ui/buttons/ActionButton.tsx
+++ src/components/ui/buttons/ActionButton.tsx
@@ -1,6 +1,20 @@
-const ActionButton = ({ children, onClick  }: { children: React.ReactNode, onClick?: () => void }) => {
+const ActionButton = ({
+  children,
+  onClick,
+  customClassName,
+}: {
+  children: React.ReactNode;
+  onClick?: () => void;
+  customClassName?: string;
+}) => {
   return (
-    <button onClick={onClick} className="bg-brand-500 flex items-center gap-2 md:gap-3 hover:bg-brand-600 text-white font-semibold py-2 md:py-3 px-4 md:px-8 rounded-full shadow-lg transition duration-300">
+    <button
+      onClick={onClick}
+      className={
+        customClassName ||
+        "bg-brand-500 flex items-center gap-2 md:gap-3 hover:bg-brand-600 text-white font-semibold py-2 md:py-3 px-4 md:px-8 rounded-full shadow-lg transition duration-300"
+      }
+    >
       {children}
     </button>
   );
